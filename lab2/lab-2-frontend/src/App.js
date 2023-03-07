43ae954a955ed4faef4865a6e235cc0714e93bcd
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-03-07 21:58:48 +0000

    axios request

--- lab2/lab-2-frontend/src/App.js
+++ lab2/lab-2-frontend/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  async function getCountriesByCapital() {
+    const res = await axios.get('http://localhost:3001/country-objects/country-by-capital-city.json');
+    console.log(res.data);
+  }
+
   return (
     <div className="App">
-        
+        <button onClick={getCountriesByCapital}>Get countries by capital</button>
     </div>
   );
 }
