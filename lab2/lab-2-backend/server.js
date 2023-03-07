f57d021832c23107a7f7348fb0423a4144ff3876
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-03-07 21:58:16 +0000

    add country by capital city endpoint

--- lab2/lab-2-backend/server.js
+++ lab2/lab-2-backend/server.js
@@ -1,12 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 3001;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Country by capital request
+  if (req.url === '/country-objects/country-by-capital-city.json') {
+    fs.readFile('country-objects/country-by-capital-city.json', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(data);
+      }
+    });
+  } else {
+    // Handle other requests here
+    res.statusCode = 404;
+    res.end('Not found')
+  }
 });
 
 server.listen(port, hostname, () => {
