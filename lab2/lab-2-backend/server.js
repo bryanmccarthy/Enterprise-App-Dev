703178f56e0e56602b583fd97603c77fcccfea1d
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-03-08 21:07:40 +0000

    add endpoints

--- lab2/lab-2-backend/server.js
+++ lab2/lab-2-backend/server.js
@@ -22,10 +22,81 @@ const server = http.createServer((req, res) => {
         res.end(data);
       }
     });
-  } else {
-    // Handle other requests here
-    res.statusCode = 404;
-    res.end('Not found')
+  } 
+  
+  // Country by continent request
+  if (req.url === '/country-objects/country-by-continent.json') {
+    fs.readFile('country-objects/country-by-continent.json', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(data);
+      }
+    });
+  }
+
+  // Country by costline request
+  if (req.url === '/country-objects/country-by-costline.json') {
+    fs.readFile('country-objects/country-by-costline.json', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(data);
+      }
+    });
+  }
+
+  // Country by currency name request
+  if (req.url === '/country-objects/country-by-currency-name.json') {
+    fs.readFile('country-objects/country-by-currency-name.json', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(data);
+      }
+    });
+  }
+
+  // Country by domain tld request
+  if (req.url === '/country-objects/country-by-domain-tld.json') {
+    fs.readFile('country-objects/country-by-domain-tld.json', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(data);
+      }
+    });
+  }
+
+  // Country by flag request
+  if (req.url === '/country-objects/country-by-flag.json') {
+    fs.readFile('country-objects/country-by-flag.json', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.statusCode = 500;
+        res.end('Internal Server Error');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(data);
+      }
+    });
   }
 });
 
--- lab2/lab-2-frontend/src/App.js
+++ lab2/lab-2-frontend/src/App.js
@@ -8,9 +8,44 @@ function App() {
     console.log(res.data);
   }
 
+  async function getCountriesByContinent() {
+    const res = await axios.get('http://localhost:3001/country-objects/country-by-continent.json');
+    console.log(res.data);
+  }
+
+  async function getCountriesByCostline() {
+    const res = await axios.get('http://localhost:3001/country-objects/country-by-costline.json');
+    console.log(res.data);
+  }
+
+  async function getCountriesByCurrencyName() {
+    const res = await axios.get('http://localhost:3001/country-objects/country-by-currency-name.json');
+    console.log(res.data);
+  }
+  
+  async function getCountriesByCurrencyName() {
+    const res = await axios.get('http://localhost:3001/country-objects/country-by-currency-name.json');
+    console.log(res.data);
+  }
+
+  async function getCountriesByDomainTld() {
+    const res = await axios.get('http://localhost:3001/country-objects/country-by-domain-tld.json');
+    console.log(res.data);
+  }
+
+  async function getCountriesByFlag() {
+    const res = await axios.get('http://localhost:3001/country-objects/country-by-flag.json');
+    console.log(res.data);
+  }
+
   return (
     <div className="App">
         <button onClick={getCountriesByCapital}>Get countries by capital</button>
+        <button onClick={getCountriesByContinent}>Get countries by continent</button>
+        <button onClick={getCountriesByCostline}>Get countries by costline</button>
+        <button onClick={getCountriesByCurrencyName}>Get countries by currency name</button>
+        <button onClick={getCountriesByDomainTld}>Get countries by domain tld</button>
+        <button onClick={getCountriesByFlag}>Get countries by flag</button>
     </div>
   );
 }
