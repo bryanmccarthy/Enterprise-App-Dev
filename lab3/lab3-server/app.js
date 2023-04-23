84c48e432cb091c51d308e5700b019db1cc20bf7
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-04-23 16:12:48 +0100

    get colors endpoints

--- lab3/lab3-server/app.js
+++ lab3/lab3-server/app.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const fs = require('fs');
+
+// GET /colors
+app.get('/colors', (req, res) => {
+  fs.readFile('colors.json', (err, data) => {
+    if (err) {
+      res.status(500).send('Server error');
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+// GET /colors/:colorId
+app.get('/colors/:colorId', (req, res) => {
+  fs.readFile('colors.json', (err, data) => {
+    if (err) {
+      res.status(500).send('Server error');
+    } else {
+      const colors = JSON.parse(data);
+      const color = colors.find((color) => color.colorId === parseInt(req.params.colorId));
+      if (color) {
+        res.send(color);
+      } else {
+        res.status(404).send('Color not found');
+      }
+    }
+  });
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
