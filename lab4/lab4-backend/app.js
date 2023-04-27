3a5ae816ed74b25c2bcd372d4e29863ed9ae0d07
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-04-27 19:34:37 +0100

    connect to mongodb

--- lab4/lab4-backend/app.js
+++ lab4/lab4-backend/app.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 const port = 8080;
 
 app.use(cors());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
