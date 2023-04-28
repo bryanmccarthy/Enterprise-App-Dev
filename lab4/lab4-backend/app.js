7db751615a93ebb850ca2f1258730e88590bdf1a
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-04-28 15:08:05 +0100

    products router and model

--- lab4/lab4-backend/app.js
+++ lab4/lab4-backend/app.js
@@ -2,21 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Product = require('./models/Product');
 const app = express();
 const port = 8080;
 
 app.use(cors());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri);
+mongoose.connect(uri)
+  .then(() => console.log('Mongoose Connected'))
+  .catch(err => console.log(err));
+
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
+  console.log('Connected to MongoDB');
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const productsRouter = require('./routes/products');
+app.use('/products', productsRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
---
+++ lab4/lab4-backend/models/Product.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true },
+  discountPercentage: { type: Number },
+  stock: { type: Number, required: true },
+  brand: { type: String, required: true },
+  category: { type: String, required: true },
+  thumbnail: { type: String, required: true },
+  images: { type: Array, required: true }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
