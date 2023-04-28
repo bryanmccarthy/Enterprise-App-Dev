26456c1acb119073de231bd08cbf1a0901710a81
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-04-28 16:05:36 +0100

    json parser

--- lab4/lab4-backend/app.js
+++ lab4/lab4-backend/app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const Product = require('./models/Product');
@@ -18,8 +20,59 @@ connection.once('open', () => {
   console.log('Connected to MongoDB');
 });
 
-const productsRouter = require('./routes/products');
-app.use('/products', productsRouter);
+// Get All Products
+app.get('/products', async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+// Get a Product
+app.get('/products/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.json(product);
+});
+
+// Create a Product
+app.post('/products', jsonParser, async (req, res) => {
+  const product = new Product({
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    discountPercentage: req.body.discountPercentage,
+    stock: req.body.stock,
+    brand: req.body.brand,
+    category: req.body.category,
+    thumbnail: req.body.thumbnail,
+    images: req.body.images
+  });
+
+  await product.save();
+  res.json(product);
+});
+
+// Update a Product
+app.put('/products/:id', jsonParser, async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  product.title = req.body.title;
+  product.description = req.body.description;
+  product.price = req.body.price;
+  product.discountPercentage = req.body.discountPercentage;
+  product.stock = req.body.stock;
+  product.brand = req.body.brand;
+  product.category = req.body.category;
+  product.thumbnail = req.body.thumbnail;
+  product.images = req.body.images;
+
+  await product.save();
+  res.json(product);
+});
+
+// Delete a Product
+app.delete('/products/:id', async (req, res) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+  res.json(product);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
