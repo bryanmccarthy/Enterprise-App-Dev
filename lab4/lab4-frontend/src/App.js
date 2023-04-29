75590fc1c7096cc051f1340139f371f8048a5f99
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-04-29 17:01:09 +0100

    axios and useQuery

--- lab4/lab4-frontend/src/App.js
+++ lab4/lab4-frontend/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Products from './components/Products';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <div className="App">
       <Products />
     </div>
+    </QueryClientProvider>
   );
 }
 
--- lab4/lab4-frontend/src/components/Products.js
+++ lab4/lab4-frontend/src/components/Products.js
@@ -1,8 +1,28 @@
 import "./Products.css";
 import Options from "./Options";
 import Product from "./Product";
+import axios from "axios";
+import { useQuery } from "react-query";
+import { useState } from "react";
+
+const URL = "http://localhost:8080";
 
 function Products() {
+  const [products, setProducts] = useState([]);
+  
+  async function getProducts() {
+    const res = await axios.get(`${URL}/products`);
+    
+    if (res.status === 200) {
+      setProducts(res.data);
+    }
+  }
+
+  const { isLoading, error } = useQuery("products", getProducts);
+
+  if (isLoading) return "Loading...";
+  if (error) return "An error has occurred: " + error.message;
+
   return (
     <div className="Products">
       <Options />
